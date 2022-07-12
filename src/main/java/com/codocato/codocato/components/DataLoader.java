66e6e567d77807fd6935ec746c3491306417fe79
Mario <magoro.graphics@gmail.com>
2022-07-12 16:57:26 +0100

    Modified prompt values

--- src/main/java/com/codocato/codocato/components/DataLoader.java
+++ src/main/java/com/codocato/codocato/components/DataLoader.java
@@ -52,10 +52,10 @@ public class DataLoader implements ApplicationRunner {
         lesson2.setGame(game2);
         lessonRepository.save(lesson2);
 
-        Prompt prompt1 = new Prompt("boolean", "lesson_prompts/off-switch.svg", "box-boolean.svg","This is a Boolean. A boolean always has a value of true or false", 50, 10);
-        Prompt prompt2 = new Prompt("string", "lesson_prompts/_banana_.svg", "box-string.svg","This is a String. A string is made up of letters. It has quotation marks around it", 60, 50);
-        Prompt prompt3 = new Prompt("integer", "lesson_prompts/7-01.svg", "box-integer.svg","This is an integer. An integer is a whole number that can be positive or negative", 40, 26);
-        Prompt prompt4 = new Prompt("float", "lesson_prompts/1-5_float.svg", "box-float.svg","This is a Float. A float is a number with a decimal point", 24, 76);
+        Prompt prompt1 = new Prompt("boolean", "lesson_prompts/off-switch.svg", "box-boolean.svg","This is a Boolean. A boolean always has a value of true or false", 24, 76);
+        Prompt prompt2 = new Prompt("string", "lesson_prompts/_banana_.svg", "box-string.svg","This is a String. A string is made up of letters. It has quotation marks around it", 45, 50);
+        Prompt prompt3 = new Prompt("integer", "lesson_prompts/7-01.svg", "box-integer.svg","This is an integer. An integer is a whole number that can be positive or negative", 25, 35);
+        Prompt prompt4 = new Prompt("float", "lesson_prompts/1-5_float.svg", "box-float.svg","This is a Float. A float is a number with a decimal point", 20, 18);
         
         //Prompt exists alone
         promptRepository.save(prompt1);
